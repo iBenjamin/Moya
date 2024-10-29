6b4a42a6c01ec00a047f8d73b954fd3b5d421d44
Benjamin Wong <iwangkaimin@gmail.com>
2024-10-29 16:37:40 +0800

    refactor: :wrench:  resetup project using SPM

--- Package.swift
+++ Package.swift
@@ -1,77 +1,47 @@
-// swift-tools-version:5.3
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
-let rocketIfNeeded: [Package.Dependency]
-
-#if os(OSX) || os(Linux)
-rocketIfNeeded = [
-    .package(url: "https://github.com/shibapm/Rocket", .upToNextMajor(from: "1.2.0")) // dev
-]
-#else
-rocketIfNeeded = []
-#endif
-
 let package = Package(
-    name: "Moya",
-    platforms: [
-        .macOS(.v10_12),
-        .iOS(.v10),
-        .tvOS(.v10),
-        .watchOS(.v3)
-    ],
-    products: [
-        .library(name: "Moya", targets: ["Moya"]),
-        .library(name: "CombineMoya", targets: ["CombineMoya"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "4.0.0")), // dev
-        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.0.0")), // dev
-        .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", .upToNextMajor(from: "9.0.0")) // dev
-    ] + rocketIfNeeded,
-    targets: [
-        .target(
-            name: "Moya",
-            dependencies: [
-                .product(name: "Alamofire", package: "Alamofire")
-            ],
-            exclude: [
-                "Supporting Files/Info.plist"
-            ]
-        ),
-        .target(
-            name: "CombineMoya",
-            dependencies: [
-                "Moya"
-            ]
-        ),
-        .testTarget( // dev
-            name: "MoyaTests",  // dev
-            dependencies: [ // dev
-                "Moya", // dev
-                "CombineMoya", // dev
-                .product(name: "Quick", package: "Quick"), // dev
-                .product(name: "Nimble", package: "Nimble"), // dev
-                .product(name: "OHHTTPStubsSwift", package: "OHHTTPStubs") // dev
-            ] // dev
-        ) // dev
-    ]
+  name: "Moya",
+  platforms: [.macOS(.v10_15), .iOS(.v17), .tvOS(.v13), .watchOS(.v6), .visionOS(.v1)],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "Moya",
+      targets: ["Moya"]),
+    .library(name: "CombineMoya", targets: ["CombineMoya"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.0.0")),
+    .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "4.0.0")), // dev
+    .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.0.0")), // dev
+    .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", .upToNextMajor(from: "9.0.0")) // dev
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "Moya",
+      dependencies: [
+        .product(name: "Alamofire", package: "Alamofire")
+      ]),
+    .target(
+      name: "CombineMoya",
+      dependencies: [
+        "Moya"
+      ]
+    ),
+    .testTarget(
+      name: "MoyaTests",
+      dependencies: [
+        "Moya",
+        "CombineMoya", // dev
+        .product(name: "Quick", package: "Quick"), // dev
+        .product(name: "Nimble", package: "Nimble"), // dev
+        .product(name: "OHHTTPStubsSwift", package: "OHHTTPStubs") // dev
+      ]
+    ),
+  ]
 )
-
-#if canImport(PackageConfig)
-import PackageConfig
-
-let config = PackageConfiguration([
-    "rocket": [
-        "before": [
-            "scripts/update_changelog.sh",
-            "scripts/update_podspec.sh"
-        ],
-        "after": [
-            "rake create_release\\[\"$VERSION\"\\]",
-            "scripts/update_docs_website.sh"
-        ]
-    ]
-]).write()
-#endif
